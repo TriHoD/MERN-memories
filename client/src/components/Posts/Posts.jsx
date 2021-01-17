b8d5efaec579923b85d1a175a69bcdeeb8ba5341
Ho Dac Tri <miugypig@Hos-MacBook-Pro.local>
2021-01-16 22:24:14 -0800

    redux

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import useStyles from './styles';
 import Post from './Post/Post.jsx';
 
 const Posts = () => {
+    const classes = useStyles();
+    const posts = useSelector((state) => state.posts);
+
+    console.log(posts);
+
     return (
         <div>
             <h1>Posts</h1>
