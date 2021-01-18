fa220910ba02fdd7ac7185902fb0ce60833fe13b
Ho Dac Tri <miugypig@Hos-MacBook-Pro.local>
2021-01-18 00:04:07 -0800

    simple like function

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -13,12 +13,10 @@ export const getPosts = async (req, res) => {
 
 export const createPost = async (req, res) => {
     const post = req.body;
-
     const newPost = new PostMessage(post);
 
     try {
         await newPost.save();
-
         res.status(201).json(newPost);
     } catch (error) {
         res.status(409).json({ message: error.message });
@@ -28,7 +26,6 @@ export const createPost = async (req, res) => {
 export const updatePost = async (req, res) => {
     const { id: _id } = req.params;
     const post = req.body; 
-
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send(`No post with ${_id}`);
     const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true}); 
     res.json(updatedPost);
@@ -40,4 +37,12 @@ export const deletePost = async (req, res) => {
     await PostMessage.findByIdAndRemove(id);
     console.log('DELETE')
     res.json({ message: "Post deleted successfully" });
+};
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with ${id}`);
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+    res.json(updatedPost);
 };
